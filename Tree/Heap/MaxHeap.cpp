8228e16ae4be19e5a8b536f9c3084587322d79ff
Sadi <123616359+Sadi86@users.noreply.github.com>
2024-05-02 01:45:58 +0600

    Heap

--- Tree/Heap/MaxHeap.cpp
+++ Tree/Heap/MaxHeap.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
     C++ program for implementation of Max Heap.
     Time Complexity of heapify: O(log n)
     Time Complexity of Build Heap: O(n)
@@ -10,9 +10,9 @@ using namespace std;
 
 void heapify(int arr[], int n, int i)
 {
-    int large = i;          //  initialize large as root
-    int left = 2 * i + 1;   //  left = 2*i + 1
-    int right = 2 * i + 2;  //  right = 2*i + 2
+    int large = i;         //  initialize large as root
+    int left = 2 * i + 1;  //  left = 2*i + 1
+    int right = 2 * i + 2; //  right = 2*i + 2
 
     //  if left child is larger than root
     if (left < n && arr[left] > arr[large])
@@ -26,11 +26,11 @@ void heapify(int arr[], int n, int i)
     if (large != i)
     {
         swap(arr[i], arr[large]);
-        heapify(arr, n, large);     //  recursively heapify the affected sub-tree
+        heapify(arr, n, large); //  recursively heapify the affected sub-tree
     }
 }
 
-void HeapSort(int arr[], int n)
+void buildHeap(int arr[], int n)
 {
     // Build max heap (rearrange array)
     for (int i = n / 2 - 1; i >= 0; i--)
@@ -46,11 +46,11 @@ void printArray(int arr[], int n)
 
 int main()
 {
-    int arr[] = {1,6, 14,10, 8, 7, 9,3,2,4,16};
+    int arr[] = {1, 6, 14, 10, 8, 7, 9, 3, 2, 4, 16};
     int n = sizeof(arr) / sizeof(arr[0]);
     cout << "Given array is: \n";
     printArray(arr, n);
-    HeapSort(arr, n);
+    buildHeap(arr, n);
 
     cout << "Max heap array is: \n";
     printArray(arr, n);
