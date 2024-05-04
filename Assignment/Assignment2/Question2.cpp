43d1613ad8e74e87f7ce6db75140472a10d50f5b
Sadi <123616359+Sadi86@users.noreply.github.com>
2024-05-05 00:09:25 +0600

    Assignment2

--- Assignment/Assignment2/Question2.cpp
+++ Assignment/Assignment2/Question2.cpp
@@ -19,6 +19,9 @@ int visited[5][5] = {0, 0, 0, 0, 0,
                      0, 0, 0, 0, 0,
                      0, 0, 0, 0, 0};
 
+queue<int> r;
+queue<int> c;
+
 void dfs(int row, int col)
 {
     // Check the Boundary
@@ -28,7 +31,7 @@ void dfs(int row, int col)
     cout << row << " , " << col << endl;
     // exit point finder
     if (maze[row][col] == 2)
-        cout << "We are in the exit" << endl;
+        cout << "We are in the exit!" << endl;
 
     // color for visited
     visited[row][col] = 1;
@@ -57,48 +60,73 @@ void dfs(int row, int col)
 
 void bfs(int row, int col)
 {
-    queue <pair<int, int>> q;
-    
-    // Check the Boundary of maze
-    if (row < 0 || row > 4 || col < 0 || col > 4)
-        return;
-    cout << row << " , " << col << endl;
-    if(maze[row][col] == 2)
-        cout << "We are in the exit" << endl;
-    
-    // color for visited
-    visited[row][col] = 1;
-    // → move to right
-    if (maze[row][col + 1] != 1 && visited[row][col + 1] == 0)
+    r.push(row);
+    c.push(col);
+    int move_count = 0;
+    while (!r.empty())
     {
-        q.push(make_pair(row,col + 1));
+        row = r.front();
+        col = c.front();
+        r.pop();
+        c.pop();
+        // Check the Boundary of maze
+        if (row < 0 || row > 4 || col < 0 || col > 4)
+            continue;
+        // Check if visited
+        if (visited[row][col] == 1)
+            continue;
+
+        cout << row << " , " << col << endl;
+
+        // color for visited
+        visited[row][col] = 1;
+
+        if (maze[row][col] == 2)
+            cout << "We are in the exit!" << endl;
+
+        // → move to right
+        if (maze[row][col + 1] != 1 && visited[row][col + 1] == 0)
+        {
+            // cout << "right ";
+            r.push(row);
+            c.push(col + 1);
+            move_count++;
+        }
+        // ← move to left
+        if (maze[row][col - 1] != 1 && visited[row][col - 1] == 0)
+        {
+            // cout << "left ";
+            r.push(row);
+            c.push(col - 1);
+            move_count++;
+        }
+        // ↓ move to down
+        if (maze[row + 1][col] != 1 && visited[row + 1][col] == 0)
+        {
+            // cout << "down ";
+            r.push(row + 1);
+            c.push(col);
+            move_count++;
+        }
+        // ↑ move to up
+        if (maze[row - 1][col] != 1 && visited[row - 1][col] == 0)
+        {
+            // cout << "up ";
+            r.push(row - 1);
+            c.push(col);
+            move_count++;
+        }
     }
-    // ← move to left
-    if(maze[row][col-1] != 1 && visited[row][col-1] == 0)
-    {
-        q.push(make_pair(row,col - 1));
-    }
-    // ↓ move to down
-    if (maze[row + 1][col] != 1 && visited[row + 1][col] == 0)
-    {
-        q.push(make_pair(row + 1,col));
-    }
-    // ↑ move to up
-    if (maze[row - 1][col] != 1 && visited[row - 1][col] == 0)
-    {
-        q.push(make_pair(row - 1,col));
-    }
-    
-    bfs(q.front().first,q.front().second);
-    q.pop();
-    
-    visited[row][col] = 0;
 }
 
 int main()
 {
-    //dfs(3, 0);
-    bfs(3,0);
+    cout << "DFS" << endl;
+    dfs(3, 0);
+    cout << "\n"
+         << "=============================" << endl;
+    cout << "BFS" << endl;
+    bfs(3, 0);
 
     return 0;
 }
